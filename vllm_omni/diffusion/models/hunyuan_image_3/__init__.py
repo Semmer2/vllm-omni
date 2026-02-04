c72fefe88403a6e50142c29a8dc67fbb0f5756c0
ElleElleWu <1608928702@qq.com>
2026-02-03 22:49:55 -0800

    [Model] Support HunyuanImage3 Diffusion Model for GPU and NPU
    Co-authored-by: skf1999 <13234016272@163.com>
    Co-authored-by: Just-it <1161406585@qq.com>
    Co-authored-by: Semmer2 <semmer@live.cn>
    Signed-off-by: ElleElleWu <1608928702@qq.com>

--- vllm_omni/diffusion/models/hunyuan_image_3/__init__.py
+++ vllm_omni/diffusion/models/hunyuan_image_3/__init__.py
@@ -9,9 +9,11 @@ from vllm_omni.diffusion.models.hunyuan_image_3.hunyuan_image_3_transformer impo
 from vllm_omni.diffusion.models.hunyuan_image_3.pipeline_hunyuan_image_3 import (
     HunyuanImage3Pipeline,
 )
+from vllm_omni.diffusion.models.hunyuan_image_3.hunyuan_fused_moe import HunyuanFusedMoE
 
 __all__ = [
     "HunyuanImage3Pipeline",
     "HunyuanImage3Model",
     "HunyuanImage3Text2ImagePipeline",
+    "HunyuanFusedMoE"
 ]
