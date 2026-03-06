bac1937fe420a2c0c08647804b0eeefd4dab45ea
ElleElleWu <1608928702@qq.com>
2026-03-06 15:30:12 +0000

    fix is_moe type and threshold, add UT for is_moe, Hunyuan_fused_moe
    Signed-off-by: ElleElleWu <1608928702@qq.com>

--- vllm_omni/diffusion/models/hunyuan_image_3/__init__.py
+++ vllm_omni/diffusion/models/hunyuan_image_3/__init__.py
@@ -2,6 +2,7 @@
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 """Hunyuan Image 3 diffusion model components."""
 
+from vllm_omni.diffusion.models.hunyuan_image_3.hunyuan_fused_moe import HunyuanFusedMoE
 from vllm_omni.diffusion.models.hunyuan_image_3.hunyuan_image_3_transformer import (
     HunyuanImage3Model,
     HunyuanImage3Text2ImagePipeline,
@@ -9,11 +10,5 @@ from vllm_omni.diffusion.models.hunyuan_image_3.hunyuan_image_3_transformer impo
 from vllm_omni.diffusion.models.hunyuan_image_3.pipeline_hunyuan_image_3 import (
     HunyuanImage3Pipeline,
 )
-from vllm_omni.diffusion.models.hunyuan_image_3.hunyuan_fused_moe import HunyuanFusedMoE
 
-__all__ = [
-    "HunyuanImage3Pipeline",
-    "HunyuanImage3Model",
-    "HunyuanImage3Text2ImagePipeline",
-    "HunyuanFusedMoE"
-]
+__all__ = ["HunyuanImage3Pipeline", "HunyuanImage3Model", "HunyuanImage3Text2ImagePipeline", "HunyuanFusedMoE"]
